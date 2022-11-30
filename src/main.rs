88ab5378306c6cd3eadd44cb22a0d3a3f2a16658
Joel Wang <joel.wang@autodesk.com>
2022-11-30 18:38:48 +0800

    upload download list

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
-#[macro_use] extern crate rocket;
-use rocket::fs::TempFile;
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::NamedFile;
+use rocket::http::Status;
+use rocket::data::{Data, ToByteUnit};
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
+use std::path::PathBuf;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref CACHE_DIR: PathBuf = [env::current_dir().unwrap().to_str().unwrap(), "cache"]
+        .iter()
+        .collect::<PathBuf>();
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    CACHE_DIR.to_str().unwrap()
 }
 
-#[get("/download")]
-fn download() -> &'static str {
-    "Hello, world!"
+#[get("/file_exists/<file_name>")]
+fn file_exists(file_name: &str) -> Status {
+    // "Hello, world!"
+    let mut new_dir = CACHE_DIR.clone();
+    new_dir.push(file_name);
+    if new_dir.exists() {
+        return Status::Ok;
+    } else {
+        return Status::NotFound;
+    }
 }
 
-#[post("/upload", format = "plain", data = "<file>")]
-async fn upload(mut file: TempFile<'_>) -> std::io::Result<()> {
-    file.persist_to(permanent_location).await
+#[get("/download/<file_name>")]
+async fn download(file_name: &str) -> NamedFile {
+    let mut new_dir = CACHE_DIR.clone();
+    new_dir.push(file_name);
+    let file = NamedFile::open(new_dir).await.unwrap();
+    file
+}
+#[post("/upload/<file_name>", format = "plain", data = "<data>")]
+async fn upload(file_name: &str, mut data: Data<'_>) -> Status {
+    let mut new_dir = CACHE_DIR.clone();
+    new_dir.push(file_name);
+    let mut file = File::create(new_dir);
+    data.open(512.kibibytes()).stream_to(file).await;
+    return Status::Ok;
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, download, upload])
+    if !CACHE_DIR.exists() {
+        std::fs::create_dir(CACHE_DIR.as_path()).unwrap();
+    }
+    rocket::build().mount("/", routes![index, download, file_exists])
 }
